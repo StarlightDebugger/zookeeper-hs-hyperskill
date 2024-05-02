141ba8393163544899cbb0917ac2fb137b1bfb15
Jennifer Nordell <subdev338@gmail.com>
2024-05-02 01:20:36 -0400

    simple console log statements. Tests passing stage 1/4

--- Zookeeper (JavaScript)/task/main.js
+++ Zookeeper (JavaScript)/task/main.js
@@ -1,1 +1,5 @@
-console.log('Hello, World!')
+console.log("I love animals!");
+console.log("Let's check on the animals...");
+console.log("The deer looks fine.");
+console.log("The bat looks happy.");
+console.log("The lion looks healthy.");
